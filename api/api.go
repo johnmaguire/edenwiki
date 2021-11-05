e3ce223a13510d1ba22bc6d8e21de4e7e9da5b54
John Maguire <contact@johnmaguire.me>
2021-11-05 02:44:02 -0400

    Switch to rs/cors

--- api/api.go
+++ api/api.go
@@ -3,8 +3,8 @@ package api
 import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/cors"
 	"github.com/johnmaguire/edenwiki/git"
+	"github.com/rs/cors"
 )
 
 type handlers struct {
@@ -14,11 +14,11 @@ type handlers struct {
 func NewRouter(wiki *git.Wiki) *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Use(cors.Handler(cors.Options{
+	r.Use(cors.New(cors.Options{
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedOrigins: []string{"http://localhost:8080"},
 		MaxAge:         300, // Maximum value not ignored by any of major browsers
-	}))
+	}).Handler)
 
 	h := handlers{wiki}
 	r.Get("/page", h.listPages)
